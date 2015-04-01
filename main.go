9db305d025aa3eb8cbda19ad92788745a69db926
Justin Overfelt <joverfelt@gmail.com>
2015-03-31 20:24:40 -0400

    all tests passing

--- main.go
+++ main.go
@@ -98,16 +98,103 @@ func NewSecureWriter(w io.Writer, priv, pub *[32]byte) io.Writer {
 	return secureWriter{w, priv, pub}
 }
 
+type secureConn struct {
+	io.Reader
+	io.Writer
+	io.Closer
+}
+
 // Dial generates a private/public key pair,
 // connects to the server, perform the handshake
 // and return a reader/writer.
 func Dial(addr string) (io.ReadWriteCloser, error) {
-	return nil, nil
+	pub, priv, err := box.GenerateKey(rand.Reader)
+
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.Dial("tcp", addr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// first thing we do is send our public key
+	_, err = conn.Write(pub[:])
+
+	if err != nil {
+		return nil, err
+	}
+
+	// wait for the server's public key
+	peerPubSlice := make([]byte, 32)
+	n, err := conn.Read(peerPubSlice)
+
+	if err != nil {
+		return nil, err
+	}
+
+	peerPubSlice = peerPubSlice[:n]
+	var peerPub [32]byte
+	copy(peerPub[:], peerPubSlice)
+
+	secCon := secureConn{
+		NewSecureReader(conn, priv, &peerPub),
+		NewSecureWriter(conn, priv, &peerPub),
+		conn,
+	}
+
+	return secCon, nil
 }
 
 // Serve starts a secure echo server on the given listener.
 func Serve(l net.Listener) error {
+	pub, priv, err := box.GenerateKey(rand.Reader)
+
+	if err != nil {
+		return err
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return err
+		}
+
+		go handleConnection(conn, pub, priv)
+	}
 	return nil
+}
+
+func handleConnection(c net.Conn, pub, priv *[32]byte) error {
+	// first wait for the client's public key
+	peerPubSlice := make([]byte, 32)
+	n, err := c.Read(peerPubSlice)
+
+	if err != nil {
+		return err
+	}
+
+	peerPubSlice = peerPubSlice[:n]
+	var peerPub [32]byte
+	copy(peerPub[:], peerPubSlice)
+
+	// then, send our public key
+	_, err = c.Write(pub[:])
+
+	if err != nil {
+		return err
+	}
+
+	// now session is "secure"
+	secureReader := NewSecureReader(c, priv, &peerPub)
+	secureWriter := NewSecureWriter(c, priv, &peerPub)
+
+	// echo
+	_, err = io.Copy(secureWriter, secureReader)
+
+	return err
 }
 
 func main() {
